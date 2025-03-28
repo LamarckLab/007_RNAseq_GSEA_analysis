55b57b5e6d965cd72b59318beefab5ac25ac5549
L. Mingkai <704021302@qq.com>
2025-03-28 16:35:17 +0800

    Update GESA.R

--- GESA.R
+++ GESA.R
@@ -7,17 +7,17 @@ library(dplyr)            # 数据处理辅助
 # Step0 工作路径设置 #
 setwd("C:/Users/Lamarck/Desktop")
 
-# Step1 读取数据 (必须含ENTREZID和log2FoldChange两列) #
+# Step1 读取数据 (必须含ENTREZID和log2FoldChange两列)
 deg <- read.csv("genes_ENSEMBL_ENTREZID.csv")
 head(deg)
 
-# Step2 构建 geneList #
+# Step2 构建 geneList
 geneList <- deg$log2FoldChange
 names(geneList) <- as.character(deg$ENTREZID)
 geneList <- sort(geneList, decreasing = TRUE)
 head(geneList)
 
-# Step3 运行 GSEA 分析 #
+# Step3 运行 GSEA 分析
 
 ## KEGG 富集分析（gseKEGG）
 kegg_gsea <- gseKEGG(
@@ -44,14 +44,14 @@ go_gsea <- gseGO(
 )
 go_gsea <- setReadable(go_gsea, OrgDb = org.Hs.eg.db, keyType = "ENTREZID")
 
-# Step4 保存富集分析结果  #
+# Step4 保存富集分析结果
 kegg_result <- kegg_gsea@result
 go_result   <- go_gsea@result
 
 write.csv(kegg_result, file = "GSEA_result_KEGG_human.csv", row.names = FALSE)
 write.csv(go_result, file = "GSEA_result_GO_human.csv",   row.names = FALSE)
 
-# Step5 可视化输出 #
+# Step5 可视化输出
 
 # 画KEGG分析第一个通路的GSEA曲线图并保存为PDF
 pdf(file = "GSEA_KEGG_Enrichment_Score_Curve.pdf", width = 8, height = 6)
